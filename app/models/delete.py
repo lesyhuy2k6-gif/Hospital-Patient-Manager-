7a70b0495f362a8c8d86ab23fe4636f732c6e57f
th3dummyking <bethanhdatcb2006@gmail.com>
2025-12-05 16:13:58 +0700

    working version

--- app/models/delete.py
+++
@@ -1,7 +0,0 @@
-from db.connection import get_connection
-
-def delete_record(table, record_id):
-    conn = get_connection()
-    with conn.cursor() as cur:
-        cur.execute(f"DELETE FROM {table} WHERE id=%s", (record_id,))
-        conn.commit()
